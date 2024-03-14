372c8ea603f06534e1f5b50c031aa8e0b3bfcc59
DailyLemonJam <93944040+DailyLemonJam@users.noreply.github.com>
2024-03-14 15:26:24 +0300

    Time: 149 ms (21.01%), Space: 51.8 MB (63.04%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cs
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution
+{
+    public IList<int> Preorder(Node root)
+    {
+        var result = new List<int>();
+
+        if (root == null) return result;
+
+        var stack = new Stack<Node>();
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+            result.Add(node.val);
+            for (int i = node.children.Count - 1; i >= 0; i--)
+            {
+                if (node.children[i] != null) stack.Push(node.children[i]);
+            }
+        }
+
+        return result;
+    }
+}
